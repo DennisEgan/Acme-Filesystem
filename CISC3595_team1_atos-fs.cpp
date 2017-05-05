dce2d411fabda76e269736f7b292e3b228e2b0d4
dennis1 <dennis@logitbot.com>
2017-05-05 15:20:04 -0400

    --created driver file in CISC blah blah blah --made UI a class because who knows --editted makefile to reflect changes made to files

---
+++ CISC3595_team1_atos-fs.cpp
@@ -0,0 +1,16 @@
+/*
+ * Driver for team #1's ATOS FS
+ *  Dennis Egan,
+ *  Justin Lesko,
+ *  Armando Califano
+ *
+ */
+
+#include "UI.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+    UI app;
+    app.run();
+}
